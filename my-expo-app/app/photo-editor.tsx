e24487e9c47c34df8c08f49125d69cabff9bbb4f
harbir <harbirbajwa@hotmail.com>
2025-04-08 23:04:50 -0700

    added basic modal for stickers

--- my-expo-app/app/photo-editor.tsx
+++ my-expo-app/app/photo-editor.tsx
@@ -1,13 +1,23 @@
 import { usePreventRemove } from "@react-navigation/native";
 import { useRouter } from "expo-router";
-import { View, Text, Image, Button, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet,
+  Pressable,
+  Modal,
+} from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useState } from "react";
 
 const richmond = require("../assets/images/IMG_1014.jpg");
 
 export default function PhotoEditor() {
   const router = useRouter();
+  const [showStickerModal, setShowStickerModal] = useState(false);
 
   // usePreventRemove(true, () => {
   //   alert("Are you sure you want to leave?");
@@ -19,9 +29,30 @@ export default function PhotoEditor() {
       <Pressable style={styles.backButton} onPress={() => router.back()}>
         <Ionicons name="arrow-back-outline" size={24} color="black" />
       </Pressable>
-      <Pressable style={styles.stickerButton} onPress={() => alert(":3")}>
+      <Pressable
+        style={styles.stickerButton}
+        onPress={() => setShowStickerModal(true)}
+      >
         <MaterialCommunityIcons name="sticker-emoji" size={24} color="black" />
       </Pressable>
+      <Modal
+        visible={showStickerModal}
+        onRequestClose={() => setShowStickerModal(false)}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setShowStickerModal(false)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -54,5 +85,47 @@ const styles = StyleSheet.create({
     right: 20,
     padding: 10,
     borderRadius: 99,
+  },
+
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "gainsboro",
+    borderRadius: 20,
+    padding: 50,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
   },
 });
