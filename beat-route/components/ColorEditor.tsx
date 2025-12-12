355436278795a189861eda860a9ecb1b30f8137b
Harbir Bajwa <harbirbajwa@hotmail.com>
2025-12-11 17:29:12 -0800

    color picker styling

--- beat-route/components/ColorEditor.tsx
+++ beat-route/components/ColorEditor.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, StyleSheet } from "react-native";
 import ColorPicker, { Panel5 } from "reanimated-color-picker";
 import { StickerStyle } from "./Stickers";
 
@@ -9,15 +9,33 @@ type ColorEditorProps = {
 
 export default function ColorEditor({ style, setStyle }: ColorEditorProps) {
   return (
-    <View>
+    <View style={styles.container}>
       <ColorPicker
         value={style.color}
         onChangeJS={(c) => {
           setStyle({ ...style, color: c.hex });
         }}
+        style={styles.picker}
       >
-        <Panel5 />
+        <Panel5 style={styles.panel} />
       </ColorPicker>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  picker: {
+    width: "95%",
+  },
+  panel: {
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.08,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+});
