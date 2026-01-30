918aa60e4a712d46cccb2e2f00d9dbd131fb85b0
Harbir Bajwa <harbirbajwa@hotmail.com>
2026-01-29 20:56:01 -0800

    chore: update Supabase client configuration and add auto-refresh session logic

--- beat-route/lib/supabase.ts
+++ beat-route/lib/supabase.ts
@@ -1,17 +1,12 @@
+import { AppState, Platform } from "react-native";
+import "react-native-url-polyfill/auto";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient } from "@supabase/supabase-js";
-import { Platform } from "react-native";
-import Constants from "expo-constants";
+import { createClient, processLock } from "@supabase/supabase-js";
 
-// LOCAL DEVELOPMENT SUPABASE
-// const supabaseUrl = "http://192.168.0.69:54381";
-// const supabaseAnonKey =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0";
-
-// REMOTE SUPABASE
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmeGJyZmZneHp2Z3p2d2R4d2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NTg2NjMsImV4cCI6MjA0OTUzNDY2M30.EMqa1C3gremVWuqHVc59zv96-H1z9HnZyCrzLEhxwXY";
-const supabaseUrl = "https://rfxbrffgxzvgzvwdxwhh.supabase.co";
+const supabaseUrl = process.env.LOCAL_SUPABASE_URL ||
+  process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.LOCAL_SUPABASE_ANON_KEY ||
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -19,5 +14,21 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
+    lock: processLock,
   },
 });
+
+// Tells Supabase Auth to continuously refresh the session automatically
+// if the app is in the foreground. When this is added, you will continue
+// to receive `onAuthStateChange` events with the `TOKEN_REFRESHED` or
+// `SIGNED_OUT` event if the user's session is terminated. This should
+// only be registered once.
+if (Platform.OS !== "web") {
+  AppState.addEventListener("change", (state) => {
+    if (state === "active") {
+      supabase.auth.startAutoRefresh();
+    } else {
+      supabase.auth.stopAutoRefresh();
+    }
+  });
+}
